24242830b2a47c5e7dc14cdd9967e2529cf000a9
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2021-11-05 22:28:41 -0400

    sky high in[ut is being worked on

--- Sky High The Video Game/Assets/Scripts/Input.cs
+++ Sky High The Video Game/Assets/Scripts/Input.cs
@@ -6,17 +6,38 @@ using UnityEngine.InputSystem;
 public class Input : MonoBehaviour
 {
     private Vector2 playerInput;
+    public Vector2 willPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        willPos = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (0 < playerInput.x)
+        {
+            // right
+            willPos = (new Vector2(-0.1f, 0f));
+        }
+        else if (playerInput.x < 0)
+        {
+            // left
+            willPos = (new Vector2(0.1f, 0f));
+        }
+
+        if (0 < playerInput.y)
+        {
+            willPos = (new Vector2(0f, 0.1f));
+        }
+        else if (playerInput.y < 0)
+        {
+            willPos = (new Vector2(0f, -0.1f));
+        }
+
+        transform.position = willPos;
     }
 
 
